400735c89683015f8861b44a52e2efcfadc47bf3
Alex <Magnatov2020@mail.ru>
2025-11-16 12:54:27 +0300

    Success

---
+++ yatube_api/posts/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from posts.models import Post, Comment, Group, Follow
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'pub_date', 'author', 'group')
+    search_fields = ('text',)
+    list_filter = ('pub_date', 'group')
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'post', 'author', 'text', 'created')
+    search_fields = ('text', 'author__username')
+    list_filter = ('created', 'author')
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'slug', 'description')
+    search_fields = ('title', 'description')
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'following')
+    list_filter = ('user', 'following')
+    search_fields = ('user__username', 'following__username')
+    empty_value_display = '-пусто-'
--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -49,5 +49,12 @@ class Follow(models.Model):
         constraints = [
             models.UniqueConstraint(
                 fields=['user', 'following'], name='unique_follow'
+            ),
+            models.CheckConstraint(
+                check=~models.Q(user=models.F('following')),
+                name='prevent_self_follow'
             )
         ]
+
+    def __str__(self):
+        return f'{self.user} follows {self.following}'
